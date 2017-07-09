7cc8a89491486b9d8a412733b6bf12a9986fe7b0
fumisoro <fumipumisecky@gmail.com>
2017-07-09 12:17:00 +0900

    add tv mode

--- app/src/main/java/com/example/fumiya/homeconrtoller/MainActivity.kt
+++ app/src/main/java/com/example/fumiya/homeconrtoller/MainActivity.kt
@@ -15,6 +15,7 @@ import com.github.kittinunf.fuel.core.FuelManager
 import android.content.Intent
 import android.app.PendingIntent
 import android.content.res.Resources
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,16 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(applicationContext, MainActivity::class.java)
+        val intent = Intent(applicationContext, TVActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
                 this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val notification = NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.abc_btn_switch_to_on_mtrl_00001)
                 .setContentText("内容")
                 .setContentTitle("タイトル")
                 .setContentInfo("通知欄")
                 .setTicker("通知概要")
                 .setContentIntent(pendingIntent)
+                .addAction(R.drawable.abc_spinner_mtrl_am_alpha, "tv", pendingIntent)
+                .addAction(R.drawable.abc_ic_star_black_16dp, "電on", pendingIntent)
+                .addAction(R.drawable.abc_ic_arrow_drop_right_black_24dp, "電off", pendingIntent)
 
         val manager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         manager.notify(0, notification.build())
@@ -40,11 +44,16 @@ class MainActivity : AppCompatActivity() {
 
         FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
         FuelManager.instance.basePath = "https://hooks.slack.com"
-        findViewById(R.id.tvButton).setOnClickListener {v -> postToSlack("{\"text\":\"テレビ電源\"}")}
+        findViewById(R.id.tvButton).setOnClickListener {v -> this.changeActivity()}
+        findViewById(R.id.tvPowerButton).setOnClickListener {v -> postToSlack("{\"text\":\"テレビ電源\"}")}
         findViewById(R.id.channelNextButton).setOnClickListener {v -> postToSlack("{\"text\":\"チャンネル次\"}")}
         findViewById(R.id.channelPrevButton).setOnClickListener {v -> postToSlack("{\"text\":\"チャンネル前\"}")}
         findViewById(R.id.rightOffButton).setOnClickListener {v -> postToSlack("{\"text\":\"電気消して\"}")}
         findViewById(R.id.rightOnButton).setOnClickListener {v -> postToSlack("{\"text\":\"電気つけて\"}")}
+        findViewById(R.id.airconCoolButton).setOnClickListener {v -> postToSlack("{\"text\":\"エアコン冷房\"}")}
+        findViewById(R.id.airconStopButton).setOnClickListener {v -> postToSlack("{\"text\":\"エアコン消して\"}")}
+        findViewById(R.id.volumeDownButton).setOnClickListener {v -> postToSlack("{\"text\":\"音量下\"}")}
+        findViewById(R.id.volumeUpButton).setOnClickListener {v -> postToSlack("{\"text\":\"音量上\"}")}
     }
 
     fun postToSlack(body : String){
@@ -54,5 +63,10 @@ class MainActivity : AppCompatActivity() {
             println(response.toString())
             println(result.toString())
         }
+    }
+
+    fun changeActivity(){
+        val intent = Intent(this, TVActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/fumiya/homeconrtoller/TVActivity.kt
@@ -0,0 +1,50 @@
+package com.example.fumiya.homeconrtoller
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.github.kittinunf.fuel.Fuel
+
+import kotlinx.android.synthetic.main.activity_tv.*
+
+class TVActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv)
+        setSupportActionBar(toolbar)
+
+        findViewById(R.id.tvPowerButton).setOnClickListener {v -> postToSlack("{\"text\":\"テレビ電源\"}")}
+        findViewById(R.id.tvChannelNextButton).setOnClickListener {v -> postToSlack("{\"text\":\"チャンネル次\"}")}
+        findViewById(R.id.tvChannelPrevButton).setOnClickListener {v -> postToSlack("{\"text\":\"チャンネル前\"}")}
+        findViewById(R.id.tvVolumeDownButton).setOnClickListener {v -> postToSlack("{\"text\":\"音量下げ\"}")}
+        findViewById(R.id.tvVolumenUpButton).setOnClickListener {v -> postToSlack("{\"text\":\"音量上げ\"}")}
+        findViewById(R.id.chromeCastButton).setOnClickListener {v -> postToSlack("{\"text\":\"キャスト\"}")}
+        findViewById(R.id.nSwitchButton).setOnClickListener {v -> postToSlack("{\"text\":\"switch\"}")}
+        findViewById(R.id.ps4PcButton).setOnClickListener {v -> postToSlack("{\"text\":\"ps4\"}")}
+        findViewById(R.id.NHKButton).setOnClickListener {v -> postToSlack("{\"text\":\"NHK\"}")}
+        findViewById(R.id.ETVButton).setOnClickListener {v -> postToSlack("{\"text\":\"Eテレ\"}")}
+        findViewById(R.id.TVKButton).setOnClickListener {v -> postToSlack("{\"text\":\"TVK\"}")}
+        findViewById(R.id.NTVButton).setOnClickListener {v -> postToSlack("{\"text\":\"日テレ\"}")}
+        findViewById(R.id.TVAsahiButton).setOnClickListener {v -> postToSlack("{\"text\":\"テレ朝\"}")}
+        findViewById(R.id.TBSButton).setOnClickListener {v -> postToSlack("{\"text\":\"TBS\"}")}
+        findViewById(R.id.TVTokyoButton).setOnClickListener {v -> postToSlack("{\"text\":\"テレ東\"}")}
+        findViewById(R.id.TokyoMXButton).setOnClickListener {v -> postToSlack("{\"text\":\"TokyoMX\"}")}
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun postToSlack(body : String){
+        val path = getString(R.string.slack_api_token);
+        Fuel.post(path).body(body).response { request, response, result ->
+            println(request.toString())
+            println(response.toString())
+            println(result.toString())
+        }
+    }
+
+}
